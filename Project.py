1739c72e61a1c6707f9d988067a754b503729f0f
jcoco1 <113215678+jcoco1@users.noreply.github.com>
2022-11-20 16:04:00 -0600

    initial commit

---
+++ Project.py
@@ -0,0 +1,47 @@
+class Vertex:
+    def __init__(self, value):
+        self.val = value
+        self.neighbors = []
+
+
+    def addNeighbors(self, value):
+        self.neighbors.append(value)
+
+
+    def __str__(self):
+        return str(self.val) + " is connected with: " + str([i for i in self.neighbors])
+
+
+    def getconnectedTo(self):
+        return [i for i in self.neighbors]
+
+
+    def getValue(self):
+        return self.val
+
+class Graph:
+    def __init__(self):
+        self.vertex_dict = {}
+        self.num_vertex = 0
+
+    def addVertex(self, val):
+        self.num_vertex += 1
+        new_vertex = Vertex(val)
+        self.vertex_dict[val] = new_vertex
+
+    def __contains__(self, item):
+        return item in self.vertex_dict
+
+    def getVertex(self, n):
+        if n in self.vertex_dict:
+            return self.vertex_dict[n]
+        else:
+            return None
+
+    def addEdge(self,v1,v2):
+        if v1 not in self.vertex_dict:
+            self.addVertex(v1)
+        if v2 not in self.vertex_dict:
+            self.addVertex(v2)
+
+
