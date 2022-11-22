e01d77e90e1b2b380f38ca69317e19be53c25f2c
LinuxLover3000 <charlieyonkura@gmail.com>
2022-11-22 10:35:24 -0600

    Filling and pouring simulation

--- jar.py
+++ jar.py
@@ -29,35 +29,47 @@ class Jar:
 
 
 class State:
-    def __init__(self, jar1, jar2, jar3):
-        self.jar1 = jar1
-        self.jar2 = jar2
-        self.jar3 = jar3
+    def __init__(self, jars: []):
+        self.jars = jars
         self.neighbors = []
     def __repr__(self):
-        return str([self.jar1, self.jar2, self.jar3])
+        return str(self.jars)
+    def __eq__(self, other: "State"):
+        assert len(self.jars) == len(other.jars)
+        b = True
+        for i in range(len(self.jars)):
+            if self.jars[i] != other.jars[i]:
+                b = False
+        return b
     def simulate(self):
         #fills
-        jar1n = copy.deepcopy(self.jar1)
-        jar1n.fill()
-        if self.jar1 != jar1n:
-            self.neighbors.append(State(copy.deepcopy(jar1n), copy.deepcopy(self.jar2), copy.deepcopy(self.jar3)))
-        jar2n = copy.deepcopy(self.jar2)
-        jar2n.fill()
-        if self.jar2 != jar2n:
-            self.neighbors.append(State(copy.deepcopy(self.jar1), copy.deepcopy(jar2n), copy.deepcopy(self.jar3)))
-        jar3n = copy.deepcopy(self.jar3)
-        jar3n.fill()
-        if self.jar3 != jar3n:
-            self.neighbors.append(State(copy.deepcopy(self.jar1), copy.deepcopy(self.jar2), copy.deepcopy(jar3n)))
+        for i in range(len(self.jars)):
+            possibleState = copy.deepcopy(self)
+            possibleState.jars[i].fill()
+            if self != possibleState and possibleState not in self.neighbors:
+                self.neighbors.append(possibleState)
         #empties
-        
+        for i in range(len(self.jars)):
+            possibleState = copy.deepcopy(self)
+            possibleState.jars[i].empty()
+            if self != possibleState and possibleState not in self.neighbors:
+                self.neighbors.append(possibleState)
         #pours
+        """for i in range(len(self.jars)):  
+            for 
+            possibleState = copy.deepcopy(self)
+            possibleState.jars[i].empty()
 
+            if self != possibleState and possibleState not in self.neighbors:
+                self.neighbors.append(possibleState)"""
 
 
 j1 = Jar(3,0)
-j2 = Jar(5,6)
+j2 = Jar(5,0)
 j3 = Jar(7,0)
-s = State(j1, j2, j3)
-print(s.simulate())
+s = State([j1, j2, j3])
+s.simulate()
+print(s.neighbors)
+for neighbor in s.neighbors:
+    neighbor.simulate()
+    print(neighbor.neighbors)
